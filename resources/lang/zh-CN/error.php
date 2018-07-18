cc018324368bd8380b19c5119acf6c129d494131
Maple <kfbb520@163.com>
2018-07-18 21:49:52 +0800

    update .gitignore

--- resources/lang/zh-CN/error.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Common\Extensions\Code;
-
-return [
-    Code::FAIL_TO_MESSAGE => "留言失败，请重试",
-    Code::FAIL_TO_ADD_LINK => "申请失败，请重试",
-];
--- resources/views/admin/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright by <a href="https://52zoe.com" target="_blank">{!! config('siteConfig.siteName') !!}</a></strong>
-</footer>
